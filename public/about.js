ff5721b0828dcc8596f5f4c96243f4bca169ea75
Izzcc <ialpho26@umd.edu>
2025-05-17 19:26:38 -0400

    fixed endpoint error

--- public/about.js
+++ public/about.js
@@ -1,17 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-fetch('/api/time')
-  .then(response => response.json())
-  .then(data => {
-    const timeElem = document.getElementById('time');
-    const aboutWelcome = document.getElementById('aboutWelcome');
-    
-    if (timeElem) {
-      timeElem.textContent = `The time is: ${data.serverTime}`;
-    }
-    
-    if (aboutWelcome) {
-      aboutWelcome.innerHTML = `Welcome to PropHunt - ${data.serverTime}`;
-    }
-  })
-  .catch(err => console.error('Error fetching time:', err));
+  fetch('/api/time')
+    .then(response => response.json())
+    .then(data => {
+      const rawTime = new Date(data.serverTime); 
+      const readableTime = rawTime.toLocaleString();
+      
+      const timeElem = document.getElementById('time');
+      const aboutWelcome = document.getElementById('aboutWelcome');
+
+      if (timeElem) {
+        timeElem.textContent = `The time is: ${readableTime}`;
+      }
+
+      if (aboutWelcome) {
+        aboutWelcome.innerHTML = `Welcome to PropHunt - ${readableTime}`;
+      }
+    })
+    .catch(err => console.error('Error fetching time:', err));
 });
